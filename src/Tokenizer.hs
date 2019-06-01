6c5c5390833c178c8f876d0cbea4a50020a7d4a4
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-06-01 17:25:44 +0900

    Define Tokenizer

--- src/Tokenizer.hs
+++ src/Tokenizer.hs
@@ -6,27 +6,34 @@ data TokenizeError = TokenizeError
   { message :: String
   , position :: Int
   }
-  
-newtype Tokenizer i a = Tokenizer { runTokenizer :: [i] -> Either TokenizeError (a, [i]) }
+
+data TokenizerState i = TokenizerState
+  { tokSource :: [i]
+  , tokPos :: Int
+  }
+
+newtype Tokenizer i a = Tokenizer
+  { runTokenizer :: TokenizerState i -> [i] -> Either TokenizeError (a, [i]) }
 
 instance Functor (Tokenizer i) where
-  fmap f p = Tokenizer $ \i -> case runTokenizer p i of
+  fmap f p = Tokenizer $ \s i -> case runTokenizer p s i of
     Right (x, o) -> Right $ (f x, o)
     Left e -> Left e
 
 instance Applicative (Tokenizer i) where
-  pure v = Tokenizer $ \i -> Right (v, i)
-  pf <*> px = Tokenizer $ \i -> case runTokenizer pf i of
-    Right (f, o) -> runTokenizer (fmap f px) o
+  pure v = Tokenizer $ \s i -> Right (v, i)
+  pf <*> px = Tokenizer $ \s i -> case runTokenizer pf s i of
+    Right (f, o) -> runTokenizer (fmap f px) s o
     Left e -> Left e
 
 instance Alternative (Tokenizer i) where
-  empty = Tokenizer . const . Left $ TokenizeError "empty" 0
-  p <|> q = Tokenizer $ \i -> case runTokenizer p i of
+  empty = Tokenizer $ \s i ->  Left $ TokenizeError "empty" 0
+  p <|> q = Tokenizer $ \s i -> case runTokenizer p s i of
     Right x -> Right x
-    Left _ -> runTokenizer q i
+    Left _ -> runTokenizer q s i
 
 instance Monad (Tokenizer i) where
-  p >>= f = Tokenizer $ \i -> case runTokenizer p i of
-    Right (x, o) -> runTokenizer (f x) o
+  p >>= f = Tokenizer $ \s i -> case runTokenizer p s i of
+    Right (x, o) -> runTokenizer (f x) s o
     Left e -> Left e
+
